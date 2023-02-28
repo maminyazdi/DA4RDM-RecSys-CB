f9288a82b9a12ffa0c1d048451c2c6d005a9e6f9
AminYazdi <yazdi@itc.rwth-aachen.de>
2023-03-01 00:31:05 +0100

    RecSys-CB

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
-from preprocessor import loadAndPreprocess_function
-from distance_similarity_calculator import result_function
-from evaluation import evaluate
+#from src.preprocessor import loadAndPreprocess_function
+#from src.distance_similarity_calculator import result_function
+from src.evaluation import evaluate
 # df = loadAndPreprocess_function(filepath="Data/tomography.csv", features=['http://purl.org/coscine/terms/sfb1394#acquiredIons', 'http://purl.org/coscine/terms/sfb1394#annularMillingParameters', 'http://purl.org/coscine/terms/sfb1394#baseTemperature', 'http://purl.org/coscine/terms/sfb1394#laserPulseEnergy', 'http://purl.org/coscine/terms/sfb1394#lowVoltageCleaning', 'http://purl.org/coscine/terms/sfb1394#pulseFrequency','http://purl.org/coscine/terms/sfb1394#runTime','http://purl.org/coscine/terms/sfb1394#specimenApexRadius'],debug=False)
 # result_function(df, resource, distanceMethod='euclidean', outputFormatJson=False, DEBUG_MODE=False)
 
-df = pd.read_csv("DataProcessed.csv")
+
+df = pd.read_csv("C:/Users/ay-admin/PycharmProjects/da4rdm-recsys-cb/Data/DataProcessed.csv")
 df.drop(columns=["Unnamed: 0"], inplace=True)
 evaluate(df)
 print("Output")
