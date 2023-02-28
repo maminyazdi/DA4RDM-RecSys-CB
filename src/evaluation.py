86491541fa13ed88386e03f347bad4e51eebca0a
Avinash Nahak <avinashnahak1993@gmail.com>
2023-02-28 18:16:08 +0100

    add evaluation function to calculate metrics

---
+++ src/evaluation.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from sklearn.metrics import confusion_matrix, precision_recall_fscore_support, mean_squared_error, roc_curve, ndcg_score
+from scipy.stats import spearmanr
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import roc_curve, auc, precision_recall_curve, average_precision_score
+from distance_similarity_calculator import result_function
+from warnings import filterwarnings
+
+
+def evaluate(df):
+    data = pd.read_csv("Data/tomography.csv", sep='|')
+    resource_list = data["Resource"].unique().tolist()
+    resource_data_dict = {}
+    for resource in resource_list:
+        matrix = result_function(df, resource, distanceMethod='euclidean', outputFormatJson=False, DEBUG_MODE=False)
+        conditions = [
+            (matrix['distance'] < 0.2),
+            (matrix['distance'] >= 0.2) & (matrix['distance'] < 0.4),
+            (matrix['distance'] >= 0.4) & (matrix['distance'] < 0.6),
+            (matrix['distance'] >= 0.6) & (matrix['distance'] < 0.8),
+            (matrix['distance'] >= 0.8)
+        ]
+        values = [5, 4, 3, 2, 1]
+        matrix['distance'] = np.select(conditions, values)
+        resource_data_dict.update({resource: matrix})
+    resource_list.append("Resource")
+
+    eval_matrix = pd.DataFrame(columns=[resource_list])
+    for resource, data in resource_data_dict.items():
+        eval_matrix.loc[-1, ["Resource"]] = resource
+        for index, rows in data.iterrows():
+            eval_matrix.loc[-1, [index]] = rows["distance"]
+        eval_matrix.index = eval_matrix.index + 1
+
+    eval_matrix.set_index('Resource', inplace=True)
+    ground_truth_matrix = eval_matrix.to_numpy(dtype=int)
+    similarity_matrix = eval_matrix.to_numpy(dtype=int)
+
+    # Compute the confusion matrix
+    confusion = confusion_matrix(ground_truth_matrix.flatten(), similarity_matrix.flatten())
+
+    # Compute precision, recall, and F1 score
+    precision, recall, f1_score, _ = precision_recall_fscore_support(ground_truth_matrix.flatten(),
+                                                                     similarity_matrix.flatten(), average='macro')
+
+    # Compute mean squared error
+    mse = mean_squared_error(ground_truth_matrix.flatten(), similarity_matrix.flatten())
+
+    # Compute NDCG score
+    ndcg = ndcg_score(ground_truth_matrix, similarity_matrix)
+
+    flat = ground_truth_matrix.flatten()
+    # Compute ROC curve
+    # fpr, tpr, _ = roc_curve(ground_truth_matrix.flatten(), similarity_matrix.flatten())
+
+    # Compute Spearman's Rank Correlation Coefficient
+    spearman_corr, _ = spearmanr(ground_truth_matrix.flatten(), similarity_matrix.flatten())
+
+    print("Confusion matrix:\n", confusion)
+    print("Precision: {:.4f}, Recall: {:.4f}, F1 score: {:.4f}".format(precision, recall, f1_score))
+    print("Mean squared error: {:.4f}".format(mse))
+    print("NDCG score: {:.4f}".format(ndcg))
+    # print("AUC score: {:.4f}".format(np.trapz(tpr, fpr)))
+    print("Spearman's Rank Correlation Coefficient: {:.4f}".format(spearman_corr))
