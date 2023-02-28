942de564a417f1f53cbd0218d2cb725a48902186
Avinash Nahak <avinashnahak1993@gmail.com>
2023-02-28 18:16:25 +0100

    Replace main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
+import pandas as pd
+
 from preprocessor import loadAndPreprocess_function
 from distance_similarity_calculator import result_function
+from evaluation import evaluate
+# df = loadAndPreprocess_function(filepath="Data/tomography.csv", features=['http://purl.org/coscine/terms/sfb1394#acquiredIons', 'http://purl.org/coscine/terms/sfb1394#annularMillingParameters', 'http://purl.org/coscine/terms/sfb1394#baseTemperature', 'http://purl.org/coscine/terms/sfb1394#laserPulseEnergy', 'http://purl.org/coscine/terms/sfb1394#lowVoltageCleaning', 'http://purl.org/coscine/terms/sfb1394#pulseFrequency','http://purl.org/coscine/terms/sfb1394#runTime','http://purl.org/coscine/terms/sfb1394#specimenApexRadius'],debug=False)
+# result_function(df, resource, distanceMethod='euclidean', outputFormatJson=False, DEBUG_MODE=False)
 
-df = loadAndPreprocess_function(filepath="tomography.csv", features=['http://purl.org/coscine/terms/sfb1394#acquiredIons', 'http://purl.org/coscine/terms/sfb1394#annularMillingParameters', 'http://purl.org/coscine/terms/sfb1394#baseTemperature', 'http://purl.org/coscine/terms/sfb1394#laserPulseEnergy', 'http://purl.org/coscine/terms/sfb1394#lowVoltageCleaning', 'http://purl.org/coscine/terms/sfb1394#pulseFrequency','http://purl.org/coscine/terms/sfb1394#runTime','http://purl.org/coscine/terms/sfb1394#specimenApexRadius'],debug=False)
-jsonOutPut = result_function(df, '1EC47F72-DF63-4D95-94E7-EB70C6BA09DB',distanceMethod='euclidean', outputFormatJson=True, DEBUG_MODE=False)
+df = pd.read_csv("DataProcessed.csv")
+df.drop(columns=["Unnamed: 0"], inplace=True)
+evaluate(df)
 print("Output")
-print(jsonOutPut)
